912777d7fd124a17faef3717aa1b7b7aa7bf53d6
Jesher Minelli <jesherdevsk8@gmail.com>
2024-07-10 16:50:24 -0300

    add unique true to email

--- application/models.py
+++ application/models.py
@@ -5,5 +5,5 @@ class UserModel(db.Document):
     cpf = db.StringField(required=True, unique=True)
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
-    email = db.EmailField(required=True)
+    email = db.EmailField(required=True, unique=True)
     birth_date = db.DateTimeField(required=True)
