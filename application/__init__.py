1e9cf3e3f8956dc5a6e9eebd3dc2ad22d70efff8
Jesher Minelli <jesherdevsk8@gmail.com>
2024-07-10 12:06:41 -0300

    first commit

---
+++ application/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+from application.db import init_db
+from application.app import User, Users
+
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    api = Api(app)
+    init_db(app)
+
+    api.add_resource(Users, '/users')
+    api.add_resource(User, '/user', '/user/<string:cpf>')
+
+    return app
+ 
